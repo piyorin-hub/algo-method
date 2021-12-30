2ca419c76b4b0c6647a2820d7a0e895a3d4d0f0d
piyorin85 <noripmark@gmail.com>
2021-12-30 22:20:04 +0900

    コミット

--- dp_pro/1-5.cpp
+++ dp_pro/1-5.cpp
@@ -6,9 +6,20 @@ int main()
 {
   int n, m;
   cin >> n >> m;
-  vector<int> a(n);
+  vector<int> a(n), t;
   for (int i = 0; i < n; i++) cin >> a[i];
 
+  t[0] = 0;
+  for (int i = 1; i < n; i++)
+  {
+    for (int j = 1; j < m; j++)
+    {
+      if (i - j >= 0)
+      {
+        t[i] = min(t[i - 1] + a[i], t[i - 2] + a[i] * j);
+      }
+    }
+  }
   
-  
+  cout << t[n - 1] << endl;
 }
